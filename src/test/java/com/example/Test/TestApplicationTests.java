39dfa7f9e43efd428f3da101467b19b20c96c426
shobinkumar <shobinjindal91@gmail.com>
2025-07-25 17:23:26 +0530

    add dummy test case

--- src/test/java/com/example/Test/TestApplicationTests.java
+++ src/test/java/com/example/Test/TestApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.Test;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,12 @@ class TestApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testOne()
+	{
+		Assertions.assertEquals(1,1);
 	}
 
 }
